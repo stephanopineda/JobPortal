0272e0692f390b7ce75cdc4499dd4df56a6c1caa
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-21 14:12:31 +0800

    dashboard

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,3 +1,7 @@
+<a href="../dashboard/dashboard.php">Dashboard</a>
 <a href="../user-listing/u.listing.php">User Listing</a>
-<a href="../user-listing/c.listing.php">Company Listing</a>
+<a href="../student-listing/s.listing.php">Student Listing</a>
+<a href="../employer-listing/e.listing.php">Employer Listing</a>
+<a href="../company-listing/c.listing.php">Company Listing</a>
+<a href="../job-listing/j.listing.php">Job Listing</a>
 <a href="../../logout.php">Logout</a>
