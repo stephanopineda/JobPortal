463fb2314f56394ae7d81a6da8f0402375fe359a
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-21 13:45:45 +0800

    admin shit

--- admin/s-admin/accounts/admin-accounts.php
+++ admin/s-admin/accounts/admin-accounts.php
@@ -1,6 +1,6 @@
 <?php
 
-include('../../connections.php');
+include('../../../connections.php');
 ?>
 
 <!DOCTYPE html>
--- admin/s-admin/navbar.php
+++ admin/s-admin/navbar.php
@@ -1,3 +1,4 @@
 <a href="../dashboard/dashboard.php">Admin Logs</a>
 <a href="../accounts/admin-accounts.php">Admin Accounts</a>
-<a href="../../logout.php">Logout</a>
+
+<a href="../../../logout.php">Logout</a>
