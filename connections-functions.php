cddb083520e8ab7aa816fe195adbf8a9ab08e452
Jan Carlo Abalos <73739927+Ariathros@users.noreply.github.com>
2023-03-21 13:33:23 +0800

    fix functions

--- connections-functions.php
+++ connections-functions.php
@@ -2,36 +2,20 @@
     function createDB($conn, $dbname){
     // Create database
     $sql = "CREATE DATABASE IF NOT EXISTS ".$dbname;
-    if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-    } else {
-    echo "Error creating database: " . $conn->error;
-    }
+    $conn->query($sql);
 
     // Create Table
     function createTable($conn, $tablename, $tablequery){
-    // sql to create table
-    $sql = "CREATE TABLE IF NOT EXISTS ".$tablename.
-    $tablequery;
-        
-        if ($conn->query($sql) === TRUE) {
-        echo "Table $tablename created successfully";
-        } else {
-        echo "Error creating table: " . $conn->error;
-        }
+        $sql = "CREATE TABLE IF NOT EXISTS ".$tablename.
+        $tablequery;
+        $conn->query($sql);
     }
-
 
     // Insert Data to Table
     function insertData($conn, $dataquery, $valuequery){
         $sql = "INSERT INTO ".$dataquery."
         VALUES ".$valuequery;
-
-        if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-        } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+        $conn->query($sql);
     }
 
     // Select Data from Table
