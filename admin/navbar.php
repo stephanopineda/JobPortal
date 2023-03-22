1a4795f45ecc70bbd82264d8f6f95d3cb1dc274e
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-23 00:08:58 +0800

    student-profile

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,5 +1,6 @@
 <a href="../dashboard/dashboard.php">Dashboard</a>
 <a href="../user-listing/u.listing.php">User Listing</a>
+<a href="../student-profile/s.profile.php">Student Profile</a>
 <a href="../application-list/a.list.php">Application Listing</a>
 <a href="../company-listing/c.listing.php">Company Listing</a>
 <a href="../job-listing/j.listing.php">Job Listing</a>
