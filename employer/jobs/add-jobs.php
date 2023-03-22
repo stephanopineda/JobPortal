40cd68afb20fb904282c326ae8540a21c1ed0bb6
lewol <76611386+eronlowell@users.noreply.github.com>
2023-03-22 22:34:52 +0800

    Update add-jobs.php

--- employer/jobs/add-jobs.php
+++ employer/jobs/add-jobs.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include 'header-link.php'; ?>
     <link rel="stylesheet" href="assets/CSS/styles.css">
-    <title>Sign Up</title>
+    <title>Add Job</title>
 </head>
 
 <body style="background-image: url('');">
