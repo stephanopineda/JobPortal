4a93cbb020def7d526533563e5b2f572033aa47c
popowgeh-chan <77272711+popowgeh-chan@users.noreply.github.com>
2023-03-23 20:11:37 +0800

    navbar and home-page student

---
+++ student/home-page.php
@@ -0,0 +1,165 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets\CSS\styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <title>PUP Job Portal</title>
+</head>
+
+<body>
+    <!--Header-->
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #800;">
+    <div class="container-fluid">
+      <a class="navbar-brand logo" href="index.html">
+        <img src="assets/jobportal_logo1.png" height="100px" width="275px"  class="d-inline-block align-text-top">
+        
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mx-auto">
+          
+          <li class="nav-item">
+            <a class="nav-link fs-3 fw-bold" href="index.html">Home</a>
+          </li>
+          <li class="nav-item mx-5">
+            <a class="nav-link  fs-3 fw-bold " href="company-profiles.html">Company Profiles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fs-3 fw-bold " href="find-job.php">Find A Job</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav d-flex flex-row-reverse">
+          <div class="dropdown">
+            <a class="btn stud-btn2 dropdown-toggle text-center" href="#" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="assets\img\no-profile.png" alt="" class="rounded-circle" height="40px">
+            </a>
+          
+            <ul class="dropdown-menu dropdown-menu-right">
+              <li><a class="dropdown-item" href="student-profile.html">Profile</a></li>
+              <li><a class="dropdown-item" href="edit-profile.html">Account Settings</a></li>
+              <li><a class="dropdown-item" href="landing-page.html">Logout</a></li>
+            </ul>
+          </div>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  <div class="">
+    <img src="#"height="1000px" width="100%"> 
+    <div class="bg"></div>
+    <div class="row">
+      <div class="col-12 text-center">
+        <img src="" alt="" width="100%">
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-5 mt-5">
+    <div class="row mx-5 mt-5">
+      <div class="col text-center">
+        <img src="#" alt="img" style="border: 0.5px solid rgba(0, 0, 0, 0.408); " width="400px" height="400px">
+      </div>
+      <div class="col">
+        <p class=" fw-bolder fs-1">The Bridge between employee and employer.</p>
+        <h4>Giving employees and companies an easier way to connect in job hunting is essential for finding the best fit for both parties. </h4>
+       
+      </div>
+    </div>
+  </div>
+
+  <div class="container mx-5 mt-5">
+    <div class="row mx-5 mt-5">
+      <div class="col-6 mt-5">
+        <p class="fw-bolder fs-1">We desire to help students start their career right!</p>
+        <h4>Students can find exciting opportunities to kickstart their career paths. As a student, finding the right job can be a daunting task, we can make it easy to explore a range of options and connect with potential employers.</h4>
+       
+      </div>
+      <div class="col-6 text-center mt-5">
+        <img src="#" alt="img" style="border: 0.5px solid rgba(0, 0, 0, 0.408); " width="400px" height="400px">
+      </div> 
+    </div>
+  </div>
+  
+  <!--Footer-->
+  <div class="container-fluid mt-5 footer border" style="border: 1px solid; background-color: #800;">
+    <div class="row">
+      <div class="col-2 my-auto text-center">
+        <img src="../views/assets/Job-Portal-icon.png" alt="img" height="20%" width="65%">
+      </div>
+      <div class="col-4 mt-2">
+        <ul class="list-unstyled">
+          <li class="">
+            <p class="footer-toc fw-bold" style=" margin-top: 25px;color:aliceblue;">Table of Contents</p>
+          </li>
+          <li class="">
+            <a class="fw-bold" href="#">Home </a>
+          </li>
+          <li class="">
+            <a class="fw-bold" href="#">Find A Job</a>
+          </li>
+          <li class="">
+            <a class="fw-bold" href="#">My Profile</a>
+          </li>
+          <li class="">
+            <a class="fw-bold" href="#">Company Profiles</a>
+          </li>
+          <li class="">
+            <a class="fw-bold" href="#">About Us</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-4 footer-two">
+        <ul class="list-unstyled">
+          <li class="">
+            <p class="footer-toc "> </p>
+          </li>
+          <li class="">
+            <a class="fw-bold"style=" margin-top: 25px; color:aliceblue;" href="https://www.facebook.com/pupcareer/">Career Guides</a>   
+            <h6 style= "color:aliceblue;"> Learn more to help you in starting your career journey!</h6>
+          </li>
+        </ul>
+      </div>
+      <div class="">
+        <p class="text-sm-center" style="color:aliceblue;">Job Portal Solutions © 2023 Grp 2 & 10</p>
+        <p class="text-sm-center" style="color:aliceblue;">All rights reserved.</p>
+      </div>
+    </div>
+
+  </div>
+            
+
+</body>
