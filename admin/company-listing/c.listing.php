d9f617d227c1b05fbd53c91c5e80326ec4d977a2
ranei <burogranei@gmail.com>
2023-03-23 21:46:44 +0800

    Update c.listing.php
    frontend admin company listing

--- admin/company-listing/c.listing.php
+++ admin/company-listing/c.listing.php
@@ -9,6 +9,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include('../../header-link.php'); ?>
+
     <title>Company Listing</title>
 </head>
 <body>
@@ -18,8 +20,135 @@
     ?>
 
     <h1>Company Listing</h1>
-    
-    <table>
+    <html lang="en">
+
+
+    <div class="container py-5">
+  <div class="row justify-content-center">
+    <h2 class="mb-4 pb-4" style="text-align: center;">Admin</h2>
+    <h2 class="mb-3 pb-3" style="text-align: left;">Company List</h2>
+
+    <div class="row mb-2 pb-2">
+      <!-- <div class="col-auto mb-2"> -->
+      <div class="col-auto">
+        <label for="entries" class="col-sm-1 col-form-label">Show</label>
+      </div>
+      <div class="col-1">
+        <input type="text" class="form-control" id="entries">
+      </div>
+      <div class="col-1 col-md-7">
+        <label for="entries" class="col-sm-1 col-form-label">entries</label>
+      </div>
+      <!-- search bar(or palitan mo nalang nung search bar na gawa nyo) -->
+      <div class="col-auto">
+        <label for="search" class="col-form-label">Search:</label>
+      </div>
+      <div class="col-auto">
+        <input type="text" id="search" class="form-control">
+      </div>
+    </div>
+
+  </div>
+
+
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Company Name</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Address</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Email Address</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Contact Number</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Contact Person</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Remarks</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Picture</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;"></font>
+          </font>
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">BPI</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Manila</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">@gmail</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">09123456789</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Juan Dela Cruz</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">old</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">.jpg</font>
+          </font>
+        </td>
+        <td>
+          <font style="vertical-align: inherit;">
+            <input style="vertical-align: inherit;" class="btn btn-primary btn-sm" type="submit" value="Edit" />
+            <input style="vertical-align: inherit;" class="btn btn-danger btn-sm mr-5" type="submit" value="Delete" />
+          </font>
+        </td>
+      </tr>
+
+      <input type="report" name="report" value="Generate Report"  class="btn btn-primary">
+
+    </tbody>
+  </table>
+</div>
+
+    <!-- <table>
 
         <thead>
             <tr>
@@ -33,7 +162,7 @@
         <tbody>
 
         </tbody>
-    </table>
+    </table> -->
 
 </body>
 </html>
