939d1afbab6f4b914973afea2efb0e8fa97faa73
ranei <burogranei@gmail.com>
2023-03-24 14:38:07 +0800

    job listing (update, delete, generate report)

---
+++ admin/job-listing/j.listing_edit_conn.php
@@ -0,0 +1,30 @@
+<?php
+
+include('../../connections.php');
+
+$jobID = $_POST['jobID'];
+$jobTitle = $_POST['jobTitle'];
+$jobSummary = $_POST['jobSummary'];
+$jobQuali = $_POST['jobQuali'];
+$jobCategory = $_POST['jobCategory'];
+$jobType = $_POST['jobType'];
+$workSetup = $_POST['workSetup'];
+$jobSalary = $_POST['jobSalary'];
+
+// Update record in database
+$sql = "UPDATE `job_list` SET `jobID`='$jobID',`jobTitle`='$jobTitle',`jobSummary`='$jobSummary',`jobQuali`='$jobQuali',`jobCategory`='$jobCategory',`jobType`='$jobType',`workSetup`='$workSetup',`jobSalary`='$jobSalary' WHERE jobID = $jobID";
+if (mysqli_query($conn, $sql)) {
+echo "<script type='text/javascript'>alert('Job Post Updated Successfully!') </script>";
+header("location:j.listing.php");
+echo "<script> </script>";
+
+
+} else {
+echo "Error updating record: " . mysqli_error($conn);
+}
+
+// Close database connection
+mysqli_close($conn);
+
+
+?>
