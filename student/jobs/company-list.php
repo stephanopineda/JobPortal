cb50aae3146688de626ac178e5d9362b7605e4f9
Ron De Leon <72436050+nightlight-22@users.noreply.github.com>
2023-03-25 02:33:04 +0800

    SearchbarICon

--- student/jobs/company-list.php
+++ student/jobs/company-list.php
@@ -50,18 +50,18 @@ include '../navbar.php';
                 <h1 class="text-left">Company List</h1>
               </div>
               <div class="col" style="padding-left: 300px; padding-top: 15px;">
-
-                <form class="form-inline" action="company-list.php" method="GET">
-
-                  <div class="form-outline">
+              <form class="form-inline" action="company-list.php" method="GET">
+                <div class="input-group">
                     <input type="search" name="search" id="search" class="form-control" placeholder="Search here.." />
-                  </div>
-
-                  <div style="padding-left:10px;">
-                    <button type="submit" class="btn btn-danger">
-                      <i class="bi bi-search"></i>
-                    </button>
-                  </div>
+                    <div class="input-group-append">
+                       <button type="submit" class="btn btn-danger">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                       <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                        </button>
+                    </div>
+                </div>    
+                      
                 </form>
               </div>
             </div>
