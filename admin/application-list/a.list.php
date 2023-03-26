fd74b4d5c99fe8eace39e27bcd5c4a49dea32196
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-27 05:52:04 +0800

    chnages

--- admin/application-list/a.list.php
+++ admin/application-list/a.list.php
@@ -21,19 +21,75 @@
         include('../admin-navbar.php');
     ?>
 
-    <h1>Application Listing</h1>
-    
-    <table>
 
-        <thead>
-            <tr>
-                <th>Application ID</th>
-                <th>Job ID</th>
-                <th>Student ID</th>
-                <th>Application Status</th>
-        </thead>
-        
-        <tbody>
+<div class="container py-5">
+    <div class="row justify-content-center">
+    <h2 class="mb-4 pb-4" style="text-align: center;"></h2>
+    <h2 class="mb-3 pb-3 fw-bold" style="text-align: left;">Application Listing</h2>
+
+    <div class="row mb-2 pb-2">
+      <!-- <div class="col-auto mb-2"> -->
+      <div class="col-auto">
+        <label for="entries" class="col-sm-1 col-form-label">Show</label>
+      </div>
+      <div class="col-1">
+        <input type="text" class="form-control" id="entries">
+      </div>
+      <div class="col-1 col-md-7">
+        <label for="entries" class="col-sm-1 col-form-label">entries</label>
+      </div>
+      <!-- search bar(or palitan mo nalang nung search bar na gawa nyo) -->
+      <div class="col-auto">
+        <label for="search" class="col-form-label">Search:</label>
+      </div>
+      <div class="col-auto">
+      <form action="" method="GET">
+        <input type="search" name="search" id="search" class="form-control">
+        <button type="submit" class="btn btn-primary">
+              <i class="fas fa-search">Search</i>
+            </button>
+          </form>
+      </div>
+    </div> <br>
+    <form action="u.listing_pdf.php" method="post">  
+      <input type="submit" name="generate_pdf" class="btn btn-success" value="Generate Report" />  
+    </form>  
+
+  </div>
+
+
+  <table class="table table-striped table-sm">
+    <thead>
+      <tr>
+      <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;"> Application ID</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;"> Job ID</font>
+          </font>
+        </th>
+
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Student ID</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Application Status</font>
+          </font>
+        </th>
+        <th scope="col">
+          <font style="vertical-align: inherit;">
+            <font style="vertical-align: inherit;">Actions</font>
+          </font>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
             <?php
             $sql = "SELECT * FROM job_applications";
             $result = $conn -> query($sql);
@@ -63,10 +119,6 @@
             }
             $conn->close();
             ?> 
-        </tbody>
-        
-        <tbody>
-
         </tbody>
     </table>
 
