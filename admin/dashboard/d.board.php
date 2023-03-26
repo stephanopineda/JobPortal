ba0f9f01c4f177478ace8c0c2d358442fa63391d
Vince Jeremy Ladion <55903157+vinceedesu@users.noreply.github.com>
2023-03-26 20:06:13 +0800

    paayos table pls

--- admin/dashboard/d.board.php
+++ admin/dashboard/d.board.php
@@ -1,25 +1,13 @@
-<?php
-    include('../../connections.php');
-    // include('../sessions-admin.php')
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php include('../../header-link.php'); ?>
-        <title>Logs</title>
-    </head>
-
-    <body>
-        <?php
-            include('../admin-navbar.php');
-        ?>
-        
-        <h1>Dashboard</h1>
-
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
     <table>
         <thead>
             <tr>
@@ -45,6 +33,5 @@
             ?>
         </tbody>
     </table>
-
-    </body>
+</body>
 </html>
